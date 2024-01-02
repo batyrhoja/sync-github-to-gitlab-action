7aa3a1f4595166aeadb884a9754d981c36bd75a9
batyrhoja <153177602+batyrhoja@users.noreply.github.com>
2024-01-02 05:32:37 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /root/.ssh
 echo "$INPUT_DESTINATION_SSH_KEY" > /root/.ssh/id_ed25519
-chmod 777 /root/.ssh/id_ed25519
+chmod 700 /root/.ssh/id_ed25519
 
 git config --global --add safe.directory /github/workspace
 git remote add mirror $INPUT_DESTINATION_REPOSITORY
